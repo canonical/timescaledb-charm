197e878d16453f92cc07b31cb358bfdd65001768
Teodor Petrican <teodor.petrican@caninical.com>
2023-03-03 14:01:26 +0200

    Introduce optional deb resources

--- README.md
+++ README.md
@@ -8,9 +8,11 @@ TimescaleDB that installs as a PostgreSQL extension. This charm is a subordinate
 postgresql charm that installs the extension and configures PostgreSQL to make it generally
 available.
 
-Please note that, at the moment, the charm will install the latest timescaledb version for the
-corresponding postgres version, by pulling the built package directly from TimescaleDB's third
-party apt repository as per [the Timescale documentation](https://docs.timescale.com/install/latest/self-hosted/installation-linux/).
+Please note that the charm will install the latest timescaledb version for the corresponding
+postgres version, by pulling the built package directly from TimescaleDB's third party apt
+repository as per [the Timescale documentation](https://docs.timescale.com/install/latest/self-hosted/installation-linux/).
+If one does not wish that, they can provide deb packages for `timescaledb-2-loader`, `timescaledb-2-postgresql`
+and `timescaledb-tools` via resources. All 3 debs must be provided in this case.
 
 ## Usage
 In order to deploy the charm, one must have a postgresql charm deployed. Please note that this
--- src/charm.py
+++ src/charm.py
@@ -7,7 +7,7 @@ from subprocess import PIPE, Popen, check_call, check_output
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, ModelError, WaitingStatus
 
 
 class TimescaleDB(CharmBase):
@@ -23,7 +23,7 @@ class TimescaleDB(CharmBase):
     def _on_install(self, event):
         installed = getattr(self.state, "installed", False)
         if installed:
-            return 
+            return
         try:
             # test if postgresql installed yet
             if not os.path.exists("/var/lib/postgresql"):
@@ -34,55 +34,42 @@ class TimescaleDB(CharmBase):
                 return
 
             # install dependencies
+            check_call(["sudo", "apt-get", "update", "-qq"])
             check_call(
                 ["sudo", "apt-get", "install", "-y", "apt-transport-https", "lsb-release", "wget"]
             )
 
-            # add apt repo to sources
-            release = check_output(["lsb_release", "-c", "-s"]).decode("utf-8").rstrip("\n")
-            ps = Popen(
-                [
-                    "echo",
-                    f"deb https://packagecloud.io/timescale/timescaledb/ubuntu/ {release} main",
-                ],
-                stdout=PIPE,
-            )
-            check_call(
-                ["sudo", "tee", "/etc/apt/sources.list.d/timescaledb.list"], stdin=ps.stdout
-            )
-            ps.wait()
+            debs = ["loader-deb", "tools-deb", "deb"]
+            deb_paths = []
 
-            # add apt key
-            ps = Popen(
-                [
-                    "wget",
-                    "--quiet",
-                    "-O",
-                    "-",
-                    "https://packagecloud.io/timescale/timescaledb/gpgkey",
-                ],
-                stdout=PIPE,
-            )
-            check_call(["sudo", "apt-key", "add", "-"], stdin=ps.stdout)
-            ps.wait()
+            for i in range(len(debs)):
+                try:
+                    path = self.model.resources.fetch(debs[i])
+                    deb_paths.append(path)
+                except NameError:
+                    self.framework.model.unit.status = BlockedStatus(
+                        f"install failed: resource '{debs[i]}' not found"
+                    )
+                    event.defer()
+                    return
+                except ModelError:
+                    pass
 
-            # install TimescaleDB
-            check_call(["sudo", "apt-get", "update", "-qq"])
-
-            if os.path.exists("/var/lib/postgresql/12"):
-                pgver = 12
-            elif os.path.exists("/var/lib/postgresql/14"):
-                pgver = 14
+            if not deb_paths:
+                self.framework.model.unit.status = MaintenanceStatus(
+                    "installing TimescaleDB from third-party repo"
+                )
+                self._install_from_repo(event)
+            elif len(deb_paths) == len(debs):
+                for dp in deb_paths:
+                    check_call(["sudo", "dpkg", "-i", dp])
             else:
-                event.framework.model.unit.status = BlockedStatus(
-                    "failed to find a compatible version of postgresql (12, 14)"
+                self.framework.model.unit.status = BlockedStatus(
+                    "install failed: missing at least one of the resources ['deb', 'loader-deb', 'tools-deb']"
                 )
                 event.defer()
                 return
 
-            check_call(
-                ["sudo", "apt-get", "install", "-y", f"timescaledb-2-postgresql-{pgver}"]
-            )
             check_call(["timescaledb-tune", "-yes"])
             check_call(["sudo", "systemctl", "restart", "postgresql"])
             event.framework.model.unit.status = ActiveStatus()
@@ -90,6 +77,46 @@ class TimescaleDB(CharmBase):
         except Exception as e:
             event.framework.model.unit.status = BlockedStatus(f"install failed: {e}")
             event.defer()
+
+    def _install_from_repo(self, event):
+        # add apt repo to sources
+        release = check_output(["lsb_release", "-c", "-s"]).decode("utf-8").rstrip("\n")
+        ps = Popen(
+            [
+                "echo",
+                f"deb https://packagecloud.io/timescale/timescaledb/ubuntu/ {release} main",
+            ],
+            stdout=PIPE,
+        )
+        check_call(["sudo", "tee", "/etc/apt/sources.list.d/timescaledb.list"], stdin=ps.stdout)
+        ps.wait()
+        # add apt key
+        ps = Popen(
+            [
+                "wget",
+                "--quiet",
+                "-O",
+                "-",
+                "https://packagecloud.io/timescale/timescaledb/gpgkey",
+            ],
+            stdout=PIPE,
+        )
+        check_call(["sudo", "apt-key", "add", "-"], stdin=ps.stdout)
+        ps.wait()
+        # install TimescaleDB
+        check_call(["sudo", "apt-get", "update", "-qq"])
+        if os.path.exists("/var/lib/postgresql/12"):
+            pgver = 12
+        elif os.path.exists("/var/lib/postgresql/14"):
+            pgver = 14
+        else:
+            event.framework.model.unit.status = BlockedStatus(
+                "failed to find a compatible version of postgresql (12, 14)"
+            )
+            event.defer()
+            return
+
+        check_call(["sudo", "apt-get", "install", "-y", f"timescaledb-2-postgresql-{pgver}"])
 
     def _on_upgrade_charm(self, event):
         installed = getattr(self.state, "installed", False)
